43fe5f851d04915d3b7f92fbedd1f46d87c67c65
Joprocorp <joprocorp@gmail.com>
2021-09-01 00:53:24 +0200

    Renaming options computed property to dateFormat

--- components/TimelineHeader.vue
+++ components/TimelineHeader.vue
@@ -13,8 +13,9 @@ export default {
     title: { type: String, required: true },
     date: { type: String, required: true },
     dateFormat: { type: Object, required: false },
-
     type: { type: String, required: false,
+
+
       validator: function (value) {
         // La valeur passée doit être l'une de ces chaines de caractères
         return ["primary", "info", "success", "warning", "danger", "link"].indexOf(value) !== -1
