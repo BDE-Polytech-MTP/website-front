43ea20c954c13acfe55d25d56ec639213bf6d170
Quentin Desbrousses <quentin.desbrousses@umontpellier.fr>
2021-09-01 17:38:27 +0200

    Adding club and clubs components

---
+++ components/Clubs.vue
@@ -0,0 +1,55 @@
+<template>
+  <section class="container">
+    <article class="section is-medium">
+      <p class="title has-text-centered" :style="headerColor">Découvrez nos clubs</p>
+      <br>
+      <div class="columns is-multiline">
+        <div class="column is-one-third" v-for="club in computedClubs" v-bind:key="club.title">
+          <Club
+            :title="club.title"
+            :image="club.image"
+            :directedBy="club.directedBy"
+            :hour="club.hour"
+            :location="club.location"
+            :color="club.color"
+          />
+        </div>
+      </div>
+    </article>
+  </section>
+</template>
+
+<script>
+export default {
+  props: {
+    clubs: { type: Array, required: true },
+    color: { type: String, default: "rgb(0,156,221)"}
+  },
+  computed: {
+    computedClubs(){
+      return this.clubs.map((e) => {
+        e.color = e.color ? e.color : this.color
+        return e
+      })
+    },
+    headerColor() {
+      return {
+        "background-color":this.color }
+    }
+  }
+};
+</script>
+
+<style lang="css" scoped>
+
+.section {
+  padding-top:60px;
+  padding-bottom: 60px;
+}
+
+p {
+  color:white;
+  border: 1px solid rgb(0,156,221);
+  border-radius: 4px;
+}
+</style>
