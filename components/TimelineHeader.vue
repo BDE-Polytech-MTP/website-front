f311de26b07d3358add6ba7e5ce17ceefef07fed
Joan TERIIHOANIA <36391019+joan-teriihoania@users.noreply.github.com>
2021-09-01 18:07:03 +0200

    Add Timeline component (#56)
    * NPM: Installing bulma-timeline extension
    
    * Adding Timeline components
    
    * Integrating Timeline component to Builder
    
    * Providing structure.timeline.json

---
+++ components/TimelineHeader.vue
@@ -0,0 +1,33 @@
+<template>
+    <header class="timeline-header">
+        <span class="tag" :class="[
+            type != undefined ? `is-${type}` : 'is-primary',
+            size != undefined ? `is-${size}` : 'is-small'
+        ]">{{ title }}</span>
+    </header>
+</template>
+
+<script>
+export default {
+  props: {
+    title: { type: String, required: true },
+    date: { type: String, required: true },
+    dateFormat: { type: Object, required: false },
+    type: { type: String, required: false, 
+      validator: function (value) {
+        // La valeur passée doit être l'une de ces chaines de caractères
+        return ["primary", "info", "success", "warning", "danger", "link"].indexOf(value) !== -1
+      }
+    },
+    size: { type: String, required: false, 
+      validator: function (value) {
+        // La valeur passée doit être l'une de ces chaines de caractères
+        return ["small", "medium", "large"].indexOf(value) !== -1
+      }
+    }
+  }
+};
+</script>
+
+<style lang="css" scoped>
+</style>
