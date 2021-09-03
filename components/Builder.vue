e595ab2b87064ba43bcf4f2fa26c6b97fe2b8025
Alexandre-Fernique <alexandre.fernique@etu.umontpellier.fr>
2021-09-03 13:02:17 +0200

    Ajout des composants pour créer un trombinoscope

--- components/Builder.vue
+++ components/Builder.vue
@@ -11,6 +11,14 @@
         :size="component.parallax.size"
         :scrollTo="component.parallax.scrollTo"
       />
+
+      <Trombinoscope
+        v-else-if="typeOf(component) =='trombinoscope'"
+        :id="component.trombinoscope.id"
+        :title="component.trombinoscope.title"
+        :membres="component.trombinoscope.membres"
+        />
+
       <div
         class="background-gradient-lines"
         v-else-if="typeOf(component) == 'article'"
@@ -85,7 +93,9 @@
 </template>
 
 <script>
+
 export default {
+
   props: {
     components: { type: Array, required: true, default: () => [] },
   },
