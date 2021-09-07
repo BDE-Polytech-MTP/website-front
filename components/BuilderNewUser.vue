adcf7b7bbfbc4e325cc4cf9941f59848259b4b9d
julien-vendran <julien.vendran@etu.umontpellier.fr>
2021-09-07 17:56:11 +0200

    Update for the validationMembre page. Admin can select multiple users to validate them. Usage of the buefy technology

--- components/BuilderNewUser.vue
+++ components/BuilderNewUser.vue
@@ -1,41 +1,58 @@
 <template>
-  <table class="table is-fullwidth">
-    <thead>
-    <tr>
-      <th>Prénom</th>
-      <th>Nom</th>
-      <th>Email</th>
-      <th>Actions</th>
-    </tr>
-    </thead>
-    <tfoot>
-    <tr>
-      <th>Prénom</th>
-      <th>Nom</th>
-      <th>Email</th>
-      <th>Actions</th>
-    </tr>
-    </tfoot>
-    <tbody>
-    <NewUser
-      v-for="(user, index) in allUsers"
-      :key="user.id"
-      :id = "user.id"
-      :firstName = "user.firstname"
-      :lastName = "user.lastname"
-      :mail = "user.email"
-    />
-    </tbody>
-  </table>
+  <section>
+    <b-field grouped group-multiline>
+      <b-button
+        label = "Désélectionner"
+        type="is-info"
+        icon-left="close"
+        class="field"
+        @click="checkedRows = []" />
+
+      <b-field>
+        <b-button
+          label = "Valider"
+          type="is-primary"
+          icon-left="close"
+          class="field"
+          @click="accept" />
+
+        <b-button
+          label = "Refuser"
+          type="is-danger"
+          icon-left="close"
+          class="field"
+          @click="decline" />
+
+      </b-field>
+    </b-field>
+
+    <b-tabs>
+      <b-tab-item label="Étudiants">
+        <b-table
+          checkable
+          :data="allUsers"
+          :columns="columns"
+          :checked-rows.sync="checkedRows"
+          :checkbox-position="checkboxPosition">
+
+          <template #bottom-left>
+            <b>Nombre d'étudiants sélectionnés </b>: {{ checkedRows.length }}
+          </template>
+        </b-table>
+      </b-tab-item>
+
+      <!--      <b-tab-item label="Checked rows">-->
+      <!--        <pre>{{ checkedRows }}</pre>-->
+      <!--      </b-tab-item>-->
+    </b-tabs>
+  </section>
 </template>
 
 <script>
 import gql from 'graphql-tag';
-import NewUser from '~/components/NewUser';
 
 export default {
   name: 'BuilderNewUser',
-  components: { NewUser },
   apollo: {
     allUsers: gql`{
       allUsers {
@@ -43,15 +60,57 @@ export default {
       }
     }`,
   },
-
   data() {
     return {
-      allUsers: {}
+      allUsers : [],
+      checkboxPosition: 'left',
+      checkedRows: [],
+      columns: [
+        {
+          field: 'firstname',
+          label: 'Prénom',
+        },
+        {
+          field: 'lastname',
+          label: 'Nom de famille',
+        },
+        {
+          field: 'email',
+          label: 'Email',
+        },
+      ]
     }
   },
-};
+
+  methods: {
+    async accept() {
+      for (let i = 0; i < this.checkedRows.length; i++) {
+        await this.$apollo.mutate({
+          mutation: gql`mutation ($mail: String!) {
+            validateUser(mail:$mail) {
+              email
+            }
+          }`,
+          variables: {
+            mail: this.checkedRows[i].email
+          }
+        });
+      }
+    },
+    async decline () {
+      for (let i = 0; i < this.checkedRows.length; i++) {
+        await this.$apollo.mutate({
+          mutation: gql`mutation ($mail: String!) {
+            deleteUser(mail:$mail) {
+              email
+            }
+          }`,
+          variables: {
+            mail: this.checkedRows[i].email
+          }
+        });
+      }
+    },
+  }
+}
 </script>
-
-<style scoped>
-
-</style>
