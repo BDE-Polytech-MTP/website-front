2a33b7074edfb66c6ede18bd15327f635ee78158
BrokenSwing <fhugouvieux@gmail.com>
2021-09-08 15:17:25 +0200

    Reformat code and remove unused import

--- components/CardTrombinoscope.vue
+++ components/CardTrombinoscope.vue
@@ -1,81 +1,73 @@
 <template>
-    <div class="card">
-      <header class="card-header">
-        <p class="card-header-title is-centered">
-          {{poste}}
+  <div class="card">
+    <header class="card-header">
+      <p class="card-header-title is-centered">
+        {{ poste }}
+      </p>
+    </header>
+    <div class="card-image">
+      <div>
+        <p>
+          <strong>{{ name }}</strong
+          ><br /><br /><br />
+          <a v-if="url" :href="url">Lien: Facebook</a>
         </p>
-      </header>
-      <div class="card-image">
-        <div >
-            <p>
-              <strong>{{name}}</strong><br><br><br>
-           <a v-if="url" :href="url">Lien: Facebook</a> </p>
-        </div>
-        <figure class="image is-2by3">
-          <img  :src="require(`~/assets/trombinoscope/${picture}`)" :alt="`Photo du trombinoscope du ${poste}`">
-        </figure>
       </div>
-
+      <figure class="image is-2by3">
+        <img
+          :src="require(`~/assets/trombinoscope/${picture}`)"
+          :alt="`Photo du trombinoscope du ${poste}`"
+        />
+      </figure>
     </div>
-
+  </div>
 </template>
 
 <script>
 export default {
-  name: "CardTrombinoscope",
-  props:{
-    poste:{type:String ,required: false , default:"Job not found"},
-    picture :{type:String, required: false,default:"error.jpg" },
-    url:{type:String,required:false},
-    name:{type:String,required:false,default:"Name not found"
-    }
-
+  name: 'CardTrombinoscope',
+  props: {
+    poste: { type: String, required: false, default: 'Job not found' },
+    picture: { type: String, required: false, default: 'error.jpg' },
+    url: { type: String, required: false },
+    name: { type: String, required: false, default: 'Name not found' },
   },
-
 };
-
-
 </script>
 
 <style scoped>
-
-.card-image:hover>div {
+.card-image:hover > div {
   visibility: visible;
   background: rgba(0, 174, 239, 0.9);
   transition-duration: 0.6s;
-
 }
-.card-image:hover>div p{
+.card-image:hover > div p {
   transform: translateY(0%);
   transition-duration: 0.6s;
 }
-a{
+a {
   color: white;
-  text-decoration:underline;
+  text-decoration: underline;
 }
-.card-image div p{
+.card-image div p {
   transform: translateY(80%);
   margin: auto;
 }
-div .card-image div{
+div .card-image div {
   position: absolute;
   visibility: hidden;
   display: flex;
   font-size: x-large;
-  color:white;
+  color: white;
   text-align: center;
   width: -webkit-fill-available;
   height: -webkit-fill-available;
-  z-index:1;
+  z-index: 1;
 }
-p strong{
+p strong {
   color: white;
 }
-.card{
+.card {
   box-shadow: 0 0 100px grey;
 }
-
-
-
-
 </style>
--- components/Trombinoscope.vue
+++ components/Trombinoscope.vue
@@ -1,10 +1,16 @@
 <template>
   <section class="container">
-    <article class="section is-medium ">
-      <h1 v-if="title" class="title is-1 has-text-centered mb-6">{{title}}</h1>
+    <article class="section is-medium">
+      <h1 v-if="title" class="title is-1 has-text-centered mb-6">
+        {{ title }}
+      </h1>
 
-      <div class="columns is-multiline is-centered ">
-        <div class="column is-one-quarter " v-for="member in members" v-bind:key="member.name">
+      <div class="columns is-multiline is-centered">
+        <div
+          class="column is-one-quarter"
+          v-for="member in members"
+          v-bind:key="member.name"
+        >
           <CardTrombinoscope
             :name="member.name"
             :picture="member.picture"
@@ -15,22 +21,17 @@
       </div>
     </article>
   </section>
-
 </template>
 
 <script>
-import CardOrganigramme from "~/components/CardTrombinoscope";
 export default {
-  name: "Trombinoscope",
-  props:{
-    title:{type:String,required: false},
-    members:{type:Array ,required:true }
-  }
-
+  name: 'Trombinoscope',
+  props: {
+    title: { type: String, required: false },
+    members: { type: Array, required: true },
+  },
 };
 </script>
 
 <style scoped>
-
-
 </style>
