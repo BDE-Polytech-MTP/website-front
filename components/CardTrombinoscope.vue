d1bcb0eba1d3fc0a4625cddf5f5d602a7a60fbb1
BrokenSwing <fhugouvieux@gmail.com>
2021-09-20 19:01:49 +0200

    Fix text display on trombinoscope cards and part of Carousel layout

--- components/CardTrombinoscope.vue
+++ components/CardTrombinoscope.vue
@@ -1,77 +1,72 @@
 <template>
-    <div class="card">
-      <header class="card-header">
-        <p class="card-header-title is-centered">
-          {{poste}}
+  <div class="card">
+    <header class="card-header">
+      <p class="card-header-title is-centered">
+        {{ poste }}
+      </p>
+    </header>
+    <div class="card-image">
+      <div class="card-desc">
+        <p class="has-text-centered">
+          <strong class="has-text-white">{{ name }}</strong>
+          <template v-if="description"> <br />{{ description }} </template>
+          <template v-if="url">
+            <br />
+            <a :href="url">Lien: Facebook</a>
+          </template>
         </p>
-      </header>
-      <div class="card-image">
-        <div>
-            <p>
-              <strong>{{name}}</strong><br>{{ description }}<br>
-           <a v-if="url" :href="url">Lien: Facebook</a> </p>
-        </div>
-        <figure class="image is-2by3">
-          <img v-if="picture" :src="require(`~/assets/img/${picture}`)" :alt="`${picture}`">
-        </figure>
       </div>
+      <figure class="image is-2by3">
+        <img
+          v-if="picture"
+          :src="require(`~/assets/img/${picture}`)"
+          :alt="`${picture}`"
+        />
+      </figure>
     </div>
-<<<<<<< HEAD
-=======
-
->>>>>>> 41dfcf0e46993ce32209000957717c9235a16d42
+  </div>
 </template>
 
 <script>
 export default {
-  props:{
-    poste:{type:String ,required: false , default:"Job not found"},
-    picture :{type:String, required: false,default:"logo380.png" },
-    url:{type:String,required:false},
-    name:{type:String,required:true,default:"Name not found" },
-    description:{type:String, required: false }
-
+  props: {
+    poste: { type: String, required: false, default: 'Job not found' },
+    picture: { type: String, required: false, default: 'logo380.png' },
+    url: { type: String, required: false },
+    name: { type: String, required: true, default: 'Name not found' },
+    description: { type: String, required: false },
   },
-
 };
-
 </script>
 
 <style scoped>
-
-.card-image:hover>div {
+.card-desc {
+  width: 100%;
+}
+.card-image:hover > .card-desc {
   visibility: visible;
   background: rgba(160, 160, 160, 0.9);
   transition-duration: 0.6s;
-
 }
-.card-image:hover>div p{
+.card-image:hover > .card-desc p {
   transform: translateY(0%);
   transition-duration: 0.6s;
 }
-a{
-  color: white;
-  text-decoration:underline;
+a {
+  text-decoration: underline;
 }
-.card-image div p{
-  transform: translateY(80%);
+.card-image .card-desc p {
+  transform: translateY(20%);
   margin: auto;
 }
-div .card-image div{
+div .card-image div {
   position: absolute;
   visibility: hidden;
   display: flex;
   font-size: x-large;
-  color:white;
-  text-align: center;
-  width: -webkit-fill-available;
-  height: -webkit-fill-available;
-  z-index:1;
+  z-index: 1;
 }
-p strong{
-  color: white;
-}
-.card{
+.card {
   box-shadow: 0 0 25px grey;
 }
 </style>
